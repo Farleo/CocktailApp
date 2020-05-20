d9644fbfdfb049905d0ba3db03163433ee989918
Taras <farleo89@gmail.com>
2020-05-20 22:01:07 +0300

    add basic cocktails list on search

--- app/src/main/java/io/devlight/cocktailapp/ui/SearchActivity.kt
+++ app/src/main/java/io/devlight/cocktailapp/ui/SearchActivity.kt
@@ -1,13 +1,15 @@
 package io.devlight.cocktailapp.ui
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import android.widget.EditText
+import android.widget.GridView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import io.devlight.cocktailapp.R
 import io.devlight.cocktailapp.api.NetworkService
 import io.devlight.cocktailapp.api.TheCocktailDbApi
 import io.devlight.cocktailapp.model.CocktailSearchResponse
-import io.devlight.cocktailapp.model.Drinks
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -15,16 +17,19 @@ import retrofit2.Response
 
 class SearchActivity : AppCompatActivity() {
     private lateinit var jsonApi: TheCocktailDbApi
-    var drinks: MutableList<Drinks> = ArrayList()
+    var drinks: MutableList<String> = ArrayList()
+    private lateinit var gridView: GridView
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
         jsonApi = NetworkService.getJSONApi()
-
+        this.gridView = findViewById(R.id.gridView)
+        val arrayAdapter: ArrayAdapter<String> =
+            ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, drinks)
+        gridView.adapter = arrayAdapter
         val editText = findViewById<EditText>(R.id.search_field)
-
         editText.addTextChangedListener(object : SearchTextWatcher() {
             override fun onSearch(text: String) {
                 search(text)
@@ -35,11 +40,17 @@ class SearchActivity : AppCompatActivity() {
         val request = jsonApi.searchCocktails(searchTag)
         request.enqueue(object : Callback<CocktailSearchResponse> {
             override fun onResponse(call: Call<CocktailSearchResponse>, response: Response<CocktailSearchResponse>) {
-                drinks.addAll(response.body()!!.drinks)
+                val result = response.body()!!.drinks.map { it.strDrink}
+                drinks.clear()
+                drinks.addAll(result.toMutableList())
             }
 
             override fun onFailure(call: Call<CocktailSearchResponse>, t: Throwable) {
-                TODO("Not yet implemented")
+                val toast = Toast.makeText(
+                    applicationContext,
+                    t.message, Toast.LENGTH_SHORT
+                )
+                toast.show()
             }
         })
     }
